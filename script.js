084befa07ce23ca27af16bc27374c0e87ac6bf3b
esuare12 <esuare12@asu.edu>
2026-07-10 00:49:20 -0400

    added assests, updated styles and content

--- script.js
+++ script.js
@@ -20,3 +20,22 @@ toggleButton.addEventListener('click', () => {
         localStorage.setItem("theme", "light");
     }
 });
+
+
+function projectTab(evt, project){
+
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for(i=0; i < tabcontent.length; i++){
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for(i=0; i < tablinks.length; i++){
+        tablinks[i].className = tablinks[1].className.replace(" active", "");
+    }
+    
+    document.getElementById(project).style.display = "block";
+    evt.currentTarget.className += " active";
+}
